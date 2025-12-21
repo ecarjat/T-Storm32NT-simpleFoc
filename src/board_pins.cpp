6798e5567e1406af10d1b3d74fb02a1f6eb0d5a4
Emmanuel Carjat <emmanuel.carjat@quanthouse.com>
2025-12-21 14:32:26 +0100

    cleanup unused functions

--- src/board_pins.cpp
+++ src/board_pins.cpp
@@ -9,16 +9,6 @@ void init_debug_led() {
   digitalWrite(STATUS_LED_PIN, LOW);
 }
 
-void init_uart_comms(unsigned long baud) {
-  init_uart_dma(baud);
-#ifdef DEBUG_SERIAL
-  // Serial debug output shares USART1 with DMA; enable only for debug builds.
-  Serial.setRx(UART_RX_PIN);
-  Serial.setTx(UART_TX_PIN);
-  Serial.begin(baud);
-#endif
-}
-
 void read_strap_pins(bool &strap0_low, bool &strap1_low) {
   pinMode(STRAP0_PIN, INPUT_PULLUP);
   pinMode(STRAP1_PIN, INPUT_PULLUP);
